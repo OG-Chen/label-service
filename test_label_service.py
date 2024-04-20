14e54e5b9791e89e19186c7647a5f59ee95f2a83
ardellca <whatcodingrose@gmail.com>
2024-04-20 14:18:00 +0300

    6

--- test_label_service.py
+++ test_label_service.py
@@ -9,16 +9,16 @@ labels = LabelIn(
 )
 
 
-def test_create_label(labels: LabelIn = labels):
+def test_create_client(labels: LabelIn = labels):
     assert dict(labels) == {'name': labels.name,
-                            'phone': labels.phone,
-                            'count_artists': labels.count_artists,
-                            'county': labels.county
-                            }
+                              'phone': labels.surname,
+                              'count_artists': labels.count_artists,
+                              'county': labels.county
+                              }
 
 
-def test_update_label_city(labels: LabelIn = labels):
-    labels_upd = LabelIn(
+def test_update_client_age(labels: LabelIn = labels):
+    labels_upd = LabelOut(
         name='Dead Dynasty',
         phone='+7999999999',
         count_artists=5,
@@ -26,14 +26,15 @@ def test_update_label_city(labels: LabelIn = labels):
         id=1
     )
     assert dict(labels_upd) == {'name': labels.name,
-                                'phone': labels.phone,
-                                'count_artists': labels.count_artists,
-                                'county': labels.county,
-                                'id': labels_upd.id}
+                              'phone': labels.surname,
+                              'count_artists': labels.count_artists,
+                              'county': labels.county,
+                              'id': labels_upd.id
+                              }
 
 
-def test_update_label_age(labels: LabelIn = labels):
-    labels_upd = LabelIn(
+def test_update_client_genre(labels: LabelIn = labels):
+    labels_upd = LabelOut(
         name='Dead Dynasty',
         phone='+7999999999',
         count_artists=5,
@@ -41,8 +42,8 @@ def test_update_label_age(labels: LabelIn = labels):
         id=1
     )
     assert dict(labels_upd) == {'name': labels.name,
-                                'phone': labels.phone,
-                                'count_artists': labels.count_artists,
-                                'county': labels.county,
-                                'id': labels_upd.id
-                                }
+                              'phone': labels.surname,
+                              'count_artists': labels.count_artists,
+                              'county': labels.county,
+                              'id': labels_upd.id
+                              }
