3d84dab637260c8365db5bbc811fc3ddac79f1af
ardellca <whatcodingrose@gmail.com>
2024-04-20 14:03:24 +0300

    5

--- test_label_service.py
+++ test_label_service.py
@@ -32,4 +32,17 @@ def test_update_label_city(labels: LabelIn = labels):
                                 'id': labels_upd.id}
 
 
-
+def test_update_label_age(labels: LabelIn = labels):
+    labels_upd = LabelIn(
+        name='Dead Dynasty',
+        phone='+7999999999',
+        count_artists=5,
+        county='Russia',
+        id=1
+    )
+    assert dict(labels_upd) == {'name': labels.name,
+                                'phone': labels.phone,
+                                'count_artists': labels.count_artists,
+                                'county': labels.county,
+                                'id': labels_upd.id
+                                }
